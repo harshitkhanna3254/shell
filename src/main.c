03789b219cd38bf0424ddb3f2d581a3462bba09e
Harshit Khanna <harshitkhanna3254@gmail.com>
2026-05-17 14:21:30 -0500

    Implement command not found handling

--- src/main.c
+++ src/main.c
@@ -6,8 +6,27 @@ int main(int argc, char *argv[])
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
 
+  char *line = NULL;
+  size_t linecap = 0;
+
+  ssize_t linelen = getline(&line, &linecap, stdin);
+
+  if (linelen == -1)
+  {
+    free(line);
+    return 0;
+  }
+
+  // Remove trailing newline
+  if (linelen > 0 && line[linelen - 1] == '\n')
+  {
+    line[linelen - 1] = '\0';
+  }
+
+  printf("%s: command not found\n", line);
+
+  free(line);
   return 0;
 }
