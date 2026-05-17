31e441a3959bbdac5f5d72670d42729c7a8cd2bc
Harshit Khanna <harshitkhanna3254@gmail.com>
2026-05-17 14:53:11 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,27 +6,31 @@ int main(int argc, char *argv[])
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
-
-  char *line = NULL;
-  size_t linecap = 0;
-
-  ssize_t linelen = getline(&line, &linecap, stdin);
-
-  if (linelen == -1)
+  while (1)
   {
+    printf("$ ");
+
+    char *line = NULL;
+    size_t linecap = 0;
+
+    ssize_t linelen = getline(&line, &linecap, stdin);
+
+    if (linelen == -1)
+    {
+      free(line);
+      break;
+    }
+
+    // Remove trailing newline
+    if (linelen > 0 && line[linelen - 1] == '\n')
+    {
+      line[linelen - 1] = '\0';
+    }
+
+    printf("%s: command not found\n", line);
+
     free(line);
-    return 0;
   }
 
-  // Remove trailing newline
-  if (linelen > 0 && line[linelen - 1] == '\n')
-  {
-    line[linelen - 1] = '\0';
-  }
-
-  printf("%s: command not found\n", line);
-
-  free(line);
   return 0;
 }
